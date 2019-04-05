fcf3d7b7649f8f4ad4ad69ee331ae48f444765d4
rolence0515 <rolence0515@gmail.com>
2019-04-05 18:26:37 +0800

    update

--- static/js/v-practice.js
+++ static/js/v-practice.js
@@ -29,8 +29,9 @@ Vue.component("v-practice", {
         <div class="">
             <el-checkbox v-model="chk" @change="oncheck_chage">完成</el-checkbox>
         </div>
-        <div> <el-button type="text" class="button" @click="oninfoclick">課程內容</el-button> 
-            
+        <div> 
+            <el-button type="text" class="button" @click="oninfoclick">課程內容</el-button> 
+            <el-button type="text" class="button" @click="onmp3click">音頻播放</el-button> 
         </div>
     </el-card>
     `,
