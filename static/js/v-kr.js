bdb69641137b90473408c1d5d0320ccf3102b9b2
rolence0515 <rolence0515@gmail.com>
2019-05-03 13:50:41 +0800

    card

--- static/js/v-kr.js
+++ static/js/v-kr.js
@@ -24,18 +24,28 @@
 
 Vue.component("v-kr", {
     template: `
+    
     <el-form-item class="mt-4" label="KR">
-        <el-input v-model="text" type="textarea" :autosize="true" :rows="2" placeholder="請輸入具體、可衡量、好記憶的關鍵成果">
-        </el-input>
-        <el-row class="mt-2">
-                <el-button @click="ishelp = !ishelp" class="mr-2 mt-1" size="mini" style="float:left;margin-top: 6px;"  :type="ishelp ? 'danger' :''" icon="el-icon-s-help" round>要救援</el-button>
-                <el-select v-model="members" class="mr-2" size="mini" style="float:left"  multiple placeholder="負責人">
+    <el-input v-model="text" type="textarea" :autosize="true" :rows="2" placeholder="請輸入具體、可衡量、好記憶的關鍵成果">
+    </el-input>
+    <el-row class="mt-2">
+        <el-col :xs="24" :sm="24" :md="20" :lg="20" :xl="20">
+                <el-button @click="ishelp = !ishelp" class="mr-2 mt-1" size="mini" style="float:left;margin-top: 6px;" :type="ishelp ? 'danger' :''"
+                    icon="el-icon-s-help" round>要救援</el-button>
+                <el-select v-model="members" class="mr-2" size="mini" style="float:left" multiple placeholder="負責人">
                     <el-option v-for="m in all_members" :key="m" :label="m" :value="m">
                     </el-option>
                 </el-select>
                 <el-rate v-model="rate" class="mt-2" style="float:left" :show-text="true" :texts="['準備中','剛開始','進行中','快完成了','順利完成']" show-text>
                 </el-rate>
-        </el-row>
+        </el-col>
+        <el-col :xs="24" :sm="24" :md="4" :lg="4" :xl="4">
+                <el-button @click="ishelp = !ishelp" class="mr-2 mt-1" size="mini" style="float:right;margin-top: 6px;" 
+                icon="el-icon-plus" round></el-button>
+                <el-button @click="ishelp = !ishelp" class="mr-2 mt-1" size="mini" style="float:right;margin-top: 6px;" 
+                icon="el-icon-delete" round></el-button>
+        </el-col>
+    </el-row>
     </el-form-item>
     `,
     props:{
